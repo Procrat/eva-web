3c054f11a622b051a40abfc0f9e9002dc5ca37d7
Stijn Seghers <stijnseghers@gmail.com>
2017-10-14 20:46:17 +1300

    Add logo

--- docs/src/App.vue
+++ docs/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <el-row :gutter="15">
+    <el-row class="logo">
+      <img src="src/assets/logo.png"/>
+    </el-row>
+
+    <el-row :gutter="20" class="content">
       <el-col id="sidebar" :span="5">
         <el-card>
           <el-form class="add-task-form">
@@ -360,8 +364,35 @@ export default {
 
 
 <style lang="sass">
-*
+
+@import 'reset.sass'
+
+$accent-colour: #00AEEF
+
+img
+  display: block
+
+html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p,
+blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img,
+ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i,
+center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption,
+tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section,
+summary, time, mark, audio, video
   font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif
+
+.logo
+  margin: 20px
+  width: calc(100% - 40px)
+  background-color: $accent-colour
+  border: 1px solid $accent-colour
+  box-shadow: 0 2px 4px 0 rgba(0,0,0,.12), 0 0 6px 0 rgba(0,0,0,.04);
+  border-radius: 6px
+  img
+    margin-top: -16px
+
+.content
+  margin: 10px !important
 
 .add-task-form
   .duration, .deadline-date, .deadline-time
--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
         exclude: /node_modules/
       },
       {
-        test: /\.css$/,
+        test: /\.(css|sass)$/,
         loader: 'style-loader!css-loader'
       },
       {
