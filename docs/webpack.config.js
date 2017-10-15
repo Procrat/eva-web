5d4de2460f3cd90ac141c1ca7b5d2c6208be0d00
Stijn Seghers <stijnseghers@gmail.com>
2017-10-15 17:53:40 +1300

    Build for Github Pages in production

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -5,7 +5,9 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: process.env.NODE_ENV === 'production'
+                  ? '/eva-web/dist/'
+                  : '/dist/',
     filename: 'frontend.js'
   },
   module: {
