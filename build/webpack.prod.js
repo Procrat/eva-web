c103a859829b8e4c6fd0ac5696a22f9a99c3a89a
Stijn Seghers <stijnseghers@gmail.com>
2019-07-30 19:05:36 +1200

    Update dependencies

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const merge = require('webpack-merge');
 const TerserPlugin = require('terser-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
